d4ccdc58ba44022070851966a447f077bd6bf8ca
sukanya <sukanyasirigiri@gmail.com>
2023-05-09 09:40:17 -0400

    servers

--- servers.tf
+++ servers.tf
@@ -1,9 +1,10 @@
 module "servers" {
-  
+  for_each = var.components
+ 
   source         = "./module"
   component_name = each.value["name"]
   env            = var.env
   instance_type  = each.value["instance_type"]
-  password       = lookup(each.value,"password", "null")
+  password       = lookup(each.value, "password", "null")
   
 }
