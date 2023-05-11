ff2957ea182f8597d2ef1c541653534721f38e7b
sukanya <sukanyasirigiri@gmail.com>
2023-05-11 13:39:59 -0400

    servers

--- servers.tf
+++ servers.tf
@@ -14,7 +14,7 @@ module "database-servers" {
 
 
 module "app-servers" {
-  depends_on = [module.database-servers]
+  depends_on = [module.database-servers ]
   for_each = var.app_servers
  
   source         = "./module"
