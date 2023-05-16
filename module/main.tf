05c11241d5b3469f918dbeaefa994d4ec486e43b
sukanya <sukanyasirigiri@gmail.com>
2023-05-16 17:40:45 -0400

    main

--- module/main.tf
+++ module/main.tf
@@ -67,9 +67,9 @@ resource "null_resource" "provisioner" {
   }
 }
 
-resource "aws_iam_role_policy" "ssm-ps-policy" {
+resource "aws_iam_policy" "ssm-ps-policy" {
   name = "${var.component_name}-${var.env}-ssm-ps-policy"
-  role = aws_iam_role.role.id
+  
 
   policy = jsonencode({
     "Version": "2012-10-17",
