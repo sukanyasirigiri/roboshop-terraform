5c6e5a603e9cfeefb03f069100d19c6e444fa43c
sukanya <sukanyasirigiri@gmail.com>
2023-05-17 16:06:14 -0400

    local

--- module/locals.tf
+++ module/locals.tf
@@ -11,4 +11,11 @@ locals {
     "ansible-pull -i localhost, -u https://github.com/sukanyasirigiri/roboshop-ansible -e env=${var.env} -e role_name=${var.component_name}"
     
   ]
+  db_tags = {
+    Name = "${var.component_name}-${var.env}"
+  }
+  app_tags = {
+    Name    = "${var.component_name}-${var.env}"
+    Monitor = "true"
+  }
 }
