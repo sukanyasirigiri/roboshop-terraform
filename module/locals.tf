c14ada6e6a2859621e035eb76c63ceda7bda7c91
sukanya <sukanyasirigiri@gmail.com>
2023-05-17 16:44:33 -0400

    locals

--- module/locals.tf
+++ module/locals.tf
@@ -2,13 +2,13 @@ locals {
   name = var.env != "" ? "${var.component_name}-${var.env}" : var.component_name
   db_commands = [
     "rm -rf roboshop-shell",
-      "git clone https://github.com/sukanyasirigiri/roboshop-shell",
+      "git clone https://github.com/sukanyasirigiri/roboshop-shell.git",
       "cd roboshop-shell",
       "sudo bash ${var.component_name}.sh ${var.password}"
   ]
   app_commands = [
    "sudo labauto ansible",
-    "ansible-pull -i localhost, -U https://github.com/sukanyasirigiri/roboshop-ansible roboshop.yml -e env=${var.env} -e role_name=${var.component_name}"
+    "ansible-pull -i localhost, -U https://github.com/sukanyasirigiri/roboshop-ansible.git roboshop.yml -e env=${var.env} -e role_name=${var.component_name}"
     
   ]
   db_tags = {
