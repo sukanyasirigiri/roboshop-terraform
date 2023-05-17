c08b080b88ebbf3400389626b81334ba1b597b09
sukanya <sukanyasirigiri@gmail.com>
2023-05-17 16:33:03 -0400

    main

--- module/locals.tf
+++ module/locals.tf
@@ -8,7 +8,7 @@ locals {
   ]
   app_commands = [
    "sudo labauto ansible",
-    "ansible-pull -i localhost, -u https://github.com/sukanyasirigiri/roboshop-ansible -e env=${var.env} -e role_name=${var.component_name}"
+    "ansible-pull -i localhost, -U https://github.com/sukanyasirigiri/roboshop-ansible roboshop.yml -e env=${var.env} -e role_name=${var.component_name}"
     
   ]
   db_tags = {
