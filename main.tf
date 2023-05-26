d311ab6a7a8afdf9c7eac8c62dcc26faff03811d
sukanya <sukanyasirigiri@gmail.com>
2023-05-26 09:27:06 -0400

    main

--- main.tf
+++ main.tf
@@ -2,5 +2,5 @@ module "vpc" {
     source = "git::https://github.com/sukanyasirigiri/tf-module-vpc.git"
 
     for_each = var.vpc
-    cidr_block = each_value["cidr_block"]
+    cidr_block = each.value["cidr_block"]
 }
