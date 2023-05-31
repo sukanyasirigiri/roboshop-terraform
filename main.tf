87310507880b8b606ee93dd2e5661875a2caebb8
sukanya <sukanyasirigiri@gmail.com>
2023-05-31 14:44:46 -0400

    vpc

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ module "app" {
 source = "git::https://github.com/sukanyasirigiri/tf-module-app.git"
 for_each = var.app
 instance_type = each.value["instance_type"]
-subnet_id = elememt(lookup(lookup(lookup(module.vpc, main, null), each.value["subnet_name"], null), "subnet_ids", null), 0)
+subnet_id = elememt(lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null), 0)
 
 }
 
