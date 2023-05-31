a724e64fc16c6b9976b900c922447d7dec2b8323
sukanya <sukanyasirigiri@gmail.com>
2023-05-31 14:37:27 -0400

    app

--- main.tf
+++ main.tf
@@ -8,6 +8,12 @@ module "vpc" {
     env = var.env 
 }
 
-output "vpc" {
-    value = module.vpc
+module "app" {
+source = "git::https://github.com/sukanyasirigiri/tf-module-app.git"
+for_each = var.app
+instance_type = each.value["instance_type"]
+subnet_id = elememt(lookup(lookup(lookup(module.vpc, main, null), each.value["subnet_name"], null), "subnet_ids", null), 0)
+
 }
+
+#element(lookup(lookup(module.vpc, each.value["subnet_name"], null), "subnet_ids", null), 0)
--- vars.tf
+++ vars.tf
@@ -1,2 +1,5 @@
 variable "env" {}
 variable "vpc" {}
+variable "app" {
+  
+}
