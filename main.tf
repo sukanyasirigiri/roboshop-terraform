1d9c871dbe42ac5a266431cab92ba29f14e1dd0b
sukanya <sukanyasirigiri@gmail.com>
2023-06-07 12:13:49 -0400

    ma

--- main.tf
+++ main.tf
@@ -20,6 +20,7 @@ min_size = each.value["min_size"]
 
 env = var.env
 bastion_cidr = var.bastion_cidr
+tags = local.tags
 
 
 subnet_ids = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
