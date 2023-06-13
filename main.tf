23df52cdb96eba8197f4fb88d9775788572f2697
sukanya <sukanyasirigiri@gmail.com>
2023-06-13 12:32:19 -0400

    var

--- main.tf
+++ main.tf
@@ -41,6 +41,8 @@ module "docdb" {
     subnets = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), "db", null), "subnet_ids", null)
     allow_db_cidr =  lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_db_cidr"], null), "subnet_cidrs", null)
     engine_version = each.value["engine_version"]
+    instance_count = each.value["instance_count"]
+    instance_class = each.value["instance_class"]
     
     
     tags = local.tags
