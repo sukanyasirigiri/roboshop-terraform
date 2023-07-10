1bab1a5179b46214ab8092159af85659d83ddda0
sukanya <sukanyasirigiri@gmail.com>
2023-07-10 14:12:58 -0400

    main

--- main.tf
+++ main.tf
@@ -78,7 +78,7 @@ module "elasticache" {
     allow_db_cidr =  lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_db_cidr"], null), "subnet_cidrs", null)
     engine_version = each.value["engine_version"]
     replicas_per_node_group = each.value["replicas_per_node_group"]
-    num_node_groups = each.value[" num_node_groups"]
+    num_node_groups = each.value["num_node_groups"]
     node_type = each.value["node_type"]
     
     
