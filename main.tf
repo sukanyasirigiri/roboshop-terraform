5ee517c1387f72e4caf70625cb42946fdf95e550
sukanya <sukanyasirigiri@gmail.com>
2023-08-08 12:34:26 -0400

    inst

--- main.tf
+++ main.tf
@@ -12,25 +12,7 @@ module "vpc" {
 
 }
 
-module "app" {
-source = "git::https://github.com/sukanyasirigiri/tf-module-app.git"
 
-for_each = var.app
-instance_type = each.value["instance_type"]
-name      = each.value["name"]
-desired_capacity      = each.value["desired_capacity"]
-max_size    = each.value["max_size"]
-min_size = each.value["min_size"]
-
-env = var.env
-bastion_cidr = var.bastion_cidr
-tags = local.tags
-
-
-subnet_ids = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
-vpc_id = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
-allow_app_cidr =  lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_app_cidr"], null), "subnet_cidrs", null)
-}
 
 
 
@@ -125,3 +107,26 @@ module "alb" {
 }
 
 
+
+module "app" {
+ dependepends_on = [module.vpc, module.docdb, module.rds, module.elasticache, module.rabbitm, module.alb]    
+source = "git::https://github.com/sukanyasirigiri/tf-module-app.git"
+
+for_each = var.app
+instance_type = each.value["instance_type"]
+name      = each.value["name"]
+desired_capacity      = each.value["desired_capacity"]
+max_size    = each.value["max_size"]
+min_size = each.value["min_size"]
+
+
+
+subnet_ids = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
+vpc_id = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+allow_app_cidr =  lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_app_cidr"], null), "subnet_cidrs", null)
+
+env = var.env
+bastion_cidr = var.bastion_cidr
+tags = local.tags
+
+}
