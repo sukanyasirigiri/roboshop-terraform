632d61e7cc0c6794b73d3da581330bd10326424c
sukanya <sukanyasirigiri@gmail.com>
2023-08-08 11:47:47 -0400

    main

--- main.tf
+++ main.tf
@@ -114,7 +114,7 @@ module "alb" {
     subnets = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), "db", null), "subnet_ids", null)
     allow_alb_cidr = each.value["name"] == "public" ? [ "0.0.0.0/0" ] : lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_alb_cidr"], null), "subnet_cidrs", null)
     name = each.value["name"]
-    internal = each.value["internal "]
+    internal = each.value["internal"]
     
     
     tags = local.tags
