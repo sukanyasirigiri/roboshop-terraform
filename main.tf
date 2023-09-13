c96e151636a145388df052375a0cbbd6e4828602
sukanya <sukanyasirigiri@gmail.com>
2023-09-13 18:07:02 -0400

    main

--- main.tf
+++ main.tf
@@ -126,6 +126,7 @@ subnet_ids = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", n
 vpc_id = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
 allow_app_cidr =  lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_app_cidr"], null), "subnet_cidrs", null)
 listener_arn =  lookup(lookup(module.alb, each.value["lb_type"], null), "listener_arn", null) 
+lb_dns_name    = lookup(lookup(module.alb, each.value["lb_type"], null), "dns_name", null)
 env = var.env
 bastion_cidr = var.bastion_cidr
 tags = local.tags
